9aefd7a1a15f5f4712ca2d8b1ed33f289e4fb163
fayzullo25 <rfayzullo123@gmail.com>
2026-09-21 07:08:08 +0500

    add my-bookings page

---
+++ client/src/pages/MyBookings.jsx
@@ -0,0 +1,11 @@
+import Title from "../components/Title"
+
+const MyBookings = () => {
+  return (
+    <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
+        <Title title="My Bookings" subtitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories." align='left'/>
+    </div>
+  )
+}
+
+export default MyBookings
