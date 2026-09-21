9e3a8e8ffca2a5241846e56d2083a18966e69d80
fayzullo25 <rfayzullo123@gmail.com>
2026-09-21 22:17:24 +0500

    fix myBookings page

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,9 +1,47 @@
 import Title from "../components/Title"
+import { assets, userBookingsDummyData } from "../assets/assets"
+import { useState } from "react"
 
 const MyBookings = () => {
+  const [bookings, setBookings] = useState(userBookingsDummyData)
   return (
     <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
-        <Title title="My Bookings" subtitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories." align='left'/>
+      <Title title="My Bookings" subtitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories." align='left' />
+      <div className="max-w-6xl mt-8 w-full text-gray-800 ">
+        <div className="hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
+          <div className="w-1/3 font-bold">Hotels</div>
+          <div className="w-1/3 font-bold">Date & Timings</div>
+          <div className="w-1/3 font-bold">Payment</div>
+        </div>
+        {
+          bookings.map((booking)=>(
+            <div key={booking._id} className="grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 py-6 first:border-t">
+              {/* Hotel Details */}
+              <div className="flex flex-col md:flex-row">
+                <img src={booking.room.images[0]} alt="hotel-img" className="min-md:w-44 rounded shadow object-cover" />
+                <div className="flex flex-col gap-1.5 max-md:mt-3 min-md:ml-4">
+                  <p className="font-playfair text-2xl">{booking.hotel.name} 
+                  <span className="font-inter text-sm">({booking.room.roomType})</span>
+                  </p>
+                  <div className="flex items-center gap-1 text-sm text-gray-500 ">
+                    <img src={assets.locationIcon} alt="location-icon"  />
+                    <span>{booking.hotel.address}</span>
+                  </div>
+                  <div className="flex items-center gap-1 text-sm text-gray-500 ">
+                    <img src={assets.guestsIcon} alt="guests-icon"  />
+                    <span>Guests:{booking.guests}</span>
+                  </div>
+                  <p className="text-base">Total: ${booking.totalPrice}</p>
+                </div>
+              </div>
+              {/* Date Timings */}
+              <div></div>
+              {/* Payment status */}
+              <div></div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
